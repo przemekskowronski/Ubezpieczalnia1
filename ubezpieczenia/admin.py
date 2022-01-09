8803fa9e16e67663da5964c3c12f658e0ae6dbfe
przemekskowronski <przemyslawc33064510@icloud.com>
2022-01-09 23:10:17 +0100

    Initial commit

---
+++ ubezpieczenia/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Ubezpieczenie
+#from .models import Ubezpieczenie, DodatkoweInfo, Ocena, Obiekt_ubezpieczony
+
+#admin.site.register(Ubezpieczenie)
+
+@admin.register(Ubezpieczenie)
+class FilmAdmin(admin.ModelAdmin):
+    # fields = ["tytul", "opis", "rok"]
+    # exclude = ["opis"]
+    list_display = ["tytul", "opis", "znizka"]
+    list_filter = ("premiera", "znizka")
+    search_fields = ("tytul", "opis")
+'''
+admin.site.register(DodatkoweInfo)
+admin.site.register(Ocena)
+admin.site.register(Obiekt_ubezpieczony)
+'''
